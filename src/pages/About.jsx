7a2d11a83e79dfdee22da1304c7e3e5a4e045e97
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-07-04 14:55:55 +0530

    about section done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,16 +22,49 @@ const About = () => {
           </p>
         </div>
       </div>
-      <div className="flex w-full md:w-[50rem] mx-auto align-middle pt-12 md:pt-16 px-4 md:px-0 justify-between">
-        <div className="flex items-center justify-center rounded-full bg-gradient-to-r from-purple-100 to-purple-700 h-[10rem] w-[10rem]">
-          <SupportIcon style={{ fontSize: 60 }} />
+      <div className="flex flex-wrap justify-center mx-auto pt-12 md:pt-16 px-4 md:px-0 ">
+        <div className="flex flex-col items-center w-full md:w-1/3 px-4 mb-8">
+          <div className="flex items-center justify-center rounded-full bg-gradient-to-r from-purple-100 to-purple-700 h-[10rem] w-[10rem]">
+            <SupportIcon style={{ fontSize: 60 }} />
+          </div>
+          <h3 className="text-black text-center font-bold text-xl p-2 m-2">
+            Support
+          </h3>
+          <p className="text-center text-gray-500 p-2 m-2">
+            Phasellus lobortis justo a magna facilisis, in commodo tellus
+            rutrum. Sed vitae condimentum nulla.
+          </p>
         </div>
-        <div className="flex items-center justify-center rounded-full bg-gradient-to-r from-purple-100 to-purple-700 h-[10rem] w-[10rem]">
-          <PhoneAndroidIcon style={{ fontSize: 60 }} />
+        <div className="flex flex-col items-center w-full md:w-1/3 px-4 mb-8">
+          <div className="flex items-center justify-center rounded-full bg-gradient-to-r from-purple-100 to-purple-700 h-[10rem] w-[10rem]">
+            <PhoneAndroidIcon style={{ fontSize: 60 }} />
+          </div>
+          <h3 className="text-black text-center font-bold text-xl p-2 m-2">
+            Cross-Platform
+          </h3>
+          <p className="text-center text-gray-500 p-2 m-2">
+            Phasellus lobortis justo a magna facilisis, in commodo tellus
+            rutrum. Sed vitae condimentum nulla.
+          </p>
         </div>
-        <div className="flex items-center justify-center rounded-full bg-gradient-to-r from-purple-100 to-purple-700 h-[10rem] w-[10rem]">
-          <BoltIcon style={{ fontSize: 60 }} />
+        <div className="flex flex-col items-center w-full md:w-1/3 px-4 mb-8">
+          <div className="flex items-center justify-center rounded-full bg-gradient-to-r from-purple-100 to-purple-700 h-[10rem] w-[10rem]">
+            <BoltIcon style={{ fontSize: 60 }} />
+          </div>
+          <h3 className="text-black text-center font-bold text-xl p-2 m-2">
+            Fast
+          </h3>
+          <p className="text-center text-gray-500 p-2 m-2">
+            Phasellus lobortis justo a magna facilisis, in commodo tellus
+            rutrum. Sed vitae condimentum nulla.
+          </p>
         </div>
+      </div>
+
+      <div className="flex justify-center pt-12 md:pt-16">
+        <button className="border-2 px-8 py-2 border-purple-700 text-black text-center rounded-md">
+          Learn More
+        </button>
       </div>
     </section>
   );
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,7 +1,15 @@
 export default function Features() {
   return (
     <section>
-      <div>Features</div>
+      <div className="mx-auto align-middle justify-center pt-12 md:pt-16 px-4 md:px-0">
+        <h1 className="text-xl text-black text-center font-bold uppercase">
+          features
+        </h1>
+        <hr className="border-purple-700 border-2 w-32 mx-auto mt-2 mb-6"></hr>
+        <h1 className="text-3xl text-black text-center font-bold uppercase">
+          Awesome Features
+        </h1>
+      </div>
     </section>
   );
 }
