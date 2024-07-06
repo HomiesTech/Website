448bc9200eef071fdf60da398f7facf06b0a639f
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-07-06 15:26:24 +0530

    button

--- src/pages/VideoSection.jsx
+++ src/pages/VideoSection.jsx
@@ -8,15 +8,33 @@ const VideoSection = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className="h-[20rem] relative flex items-center justify-center py-8 bg-purple-700 bg-opacity-10">
+    <section className="h-[20rem] relative flex items-center justify-center py-8 bg-blue-700 bg-opacity-40">
       <div className="text-center">
-        <h2 className="text-6xl font-bold text-purple-700 mb-4">Watch Now</h2>
+        <h2 className="text-6xl font-bold text-blue-700 mb-4">Demo Video</h2>
         <button
-          className="text-white bg-purple-700 p-4 rounded-full"
+          className="text-white bg-blue-700 p-4 rounded-full"
           onClick={() => setOpen(true)}
         >
           <FontAwesomeIcon icon={faPlay} size="2x" />
         </button>
+        <div className="flex flex-row">
+          <div className="mt-4 md:mt-8  m-[2rem] p-[2rem]">
+            <a
+              href="#"
+              className="bg-gradient-to-r from-blue-400 to-blue-700 text-white font-semibold py-3 px-6 rounded-full shadow-lg hover:from-blue-500 hover:to-purple-700 transition duration-300"
+            >
+              Download Application
+            </a>
+          </div>
+          <div className="mt-4 md:mt-8  m-[2rem] p-[2rem]">
+            <a
+              href="#"
+              className="bg-gradient-to-r from-blue-400 to-blue-700 text-white font-semibold py-3 px-6 rounded-full shadow-lg hover:from-blue-500 hover:to-purple-700 transition duration-300"
+            >
+              Download Application
+            </a>
+          </div>
+        </div>
       </div>
 
       <ModalVideo
