a74badf42710d5cf79773d8c99442a305589ce23
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-07-15 12:25:33 +0530

    about justification

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,26 +6,24 @@ import BoltIcon from "@mui/icons-material/Bolt";
 const About = () => {
   return (
     <section style={{ background: "white" }}>
-      <div className="mx-auto align-middle justify-center pt-16 md:mt-16 md:pt-16 px-4 md:px-0">
-        <h1 className="text-3xl text-black text-center font-bold uppercase font-poppins ">
+      <div className="max-w-6xl mx-auto pt-[17rem] mt-20 md:mt-16 md:pt-16 px-4 md:px-8">
+        <h1 className="text-3xl text-black text-center font-bold uppercase font-poppins">
           ABOUT
         </h1>
         <hr className="border-blue-600 border-2 w-32 mx-auto mt-2 mb-6"></hr>
-        <div className=" text-center mx-auto max-w-6xl text-gray-500 font-poppins">
+        <div className="text-center mx-auto text-gray-500 font-poppins">
           <p className="text-justify">
             At Homenetics, we specialize in converting ordinary homes into smart
-            home through innovative IOT solutions. Our advanced system
-            seamlessly integrates with your existing appliances, allowing you to
-            control and monitor them from a single, user-friendly mobile app. No
-            need to replace your current devices – Our Technology will upgrade
-            them, bringing new levels of automation and intelligence to your
-            living space.
+            homes through innovative IOT solutions. Our advanced system
+            seamlessly integrates with yourAt Homenetics, we specialize in
+            converting ordinary homes into smart homes through innovative IOT
           </p>
         </div>
       </div>
-      <div className="flex flex-wrap justify-center mx-auto pt-12 md:pt-16 px-4 md:px-0 font-poppins ">
+
+      <div className="max-w-6xl mx-auto flex flex-wrap justify-center pt-12 md:pt-16 px-4 md:px-8 font-poppins">
         <div className="flex flex-col items-center w-full md:w-1/3 px-4 mb-8">
-          <div className="flex text-white items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-blue-700 h-[10rem] w-[10rem]">
+          <div className="flex text-white items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-blue-700 h-[8rem] w-[8rem]">
             <SupportIcon style={{ fontSize: 60 }} />
           </div>
           <h3 className="text-black text-center font-bold text-xl p-2 m-2">
@@ -37,11 +35,11 @@ const About = () => {
           </p>
         </div>
         <div className="flex flex-col items-center w-full md:w-1/3 px-4 mb-8">
-          <div className="flex text-white items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-blue-700 h-[10rem] w-[10rem]">
+          <div className="flex text-white items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-blue-700 h-[8rem] w-[8rem]">
             <PhoneAndroidIcon style={{ fontSize: 60 }} />
           </div>
           <h3 className="text-black text-center font-bold text-xl p-2 m-2">
-            Realiable
+            Reliable
           </h3>
           <p className="text-center text-gray-500 p-2 m-2">
             Phasellus lobortis justo a magna facilisis, in commodo tellus
@@ -49,7 +47,7 @@ const About = () => {
           </p>
         </div>
         <div className="flex flex-col items-center w-full md:w-1/3 px-4 mb-8">
-          <div className="flex text-white items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-blue-700 h-[10rem] w-[10rem]">
+          <div className="flex text-white items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-blue-700 h-[8rem] w-[8rem]">
             <BoltIcon style={{ fontSize: 60 }} />
           </div>
           <h3 className="text-black text-center font-bold text-xl p-2 m-2">
@@ -62,7 +60,7 @@ const About = () => {
         </div>
       </div>
 
-      <div className="flex justify-center pt-12 md:pt-16 p-10">
+      <div className="flex justify-center pt-12 md:pt-16 px-4 md:px-8">
         <button className="border-2 font-poppins text-xl font-bold px-8 py-2 border-blue-600 text-black text-center rounded-md">
           Learn More
         </button>
