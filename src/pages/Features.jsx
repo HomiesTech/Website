a8ab0127e79f9c963693d18b59164a10dec64bb5
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-07-16 10:13:46 +0530

    Icons Changed

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -7,10 +7,16 @@ import ShieldIcon from "@mui/icons-material/Shield";
 import CloudIcon from "@mui/icons-material/Cloud";
 import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
 import DoneIcon from "@mui/icons-material/Done";
-import SettingsIcon from "@mui/icons-material/Settings";
+
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import Carousel from "./Carousel";
 
+import InstallMobileIcon from "@mui/icons-material/InstallMobile";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import Person2Icon from "@mui/icons-material/Person2";
+import WifiIcon from "@mui/icons-material/Wifi";
+import Diversity1Icon from "@mui/icons-material/Diversity1";
+import RecommendIcon from "@mui/icons-material/Recommend";
 export default function Features() {
   return (
     <section className="mt-2 pt-[1.5rem] pb-[1.5rem] bg-white">
@@ -26,37 +32,37 @@ export default function Features() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 p-4 md:p-10">
         {[
           {
-            Icon: VisibilityIcon,
+            Icon: AccessibilityIcon,
             title: "Easily Accessible",
             description:
               "Our product is easily accessible and easily installable as per the customer requirements",
           },
           {
-            Icon: LayersIcon,
+            Icon: Person2Icon,
             title: "Editable Profile",
             description:
               "The customer data (includes SignUp, Login, Personal details) are easily editable at the users end",
           },
           {
-            Icon: BorderColorIcon,
+            Icon: WifiIcon,
             title: "Multiple Connection",
             description:
               "Multiple connection were also supported by the app and the product as per the user needs",
           },
           {
-            Icon: ShieldIcon,
+            Icon: Diversity1Icon,
             title: "User Friendly",
             description:
               "The app is user friendly and can be accessible by anyone in the house very easily. Just a tap and go          ",
           },
           {
-            Icon: CloudIcon,
+            Icon: PhoneAndroidIcon,
             title: "Customizable Interface",
             description:
               "The Interface is customizable as per the user’s need. It will help out the user to name the tabs for the devices as per the user’s need",
           },
           {
-            Icon: PhoneAndroidIcon,
+            Icon: RecommendIcon,
             title: "24*7 Support System",
             description:
               "If any problem occur in between or after installation of the product our 24*7 support system will help you out. Our support team will look into the matter of the issue and then a technician will arrive to fix the issue",
@@ -125,7 +131,7 @@ export default function Features() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 m-2 p-2 mt-8 pt-8">
           {[
             {
-              Icon: SettingsIcon,
+              Icon: InstallMobileIcon,
               title: "Install The App",
               description:
                 "The app is easy to use as you just have to download the app from the website/app store/playstore create an account with username and password.Now connect the app to the dedicated WIFI and discover the device",
