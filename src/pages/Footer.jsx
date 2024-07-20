1ee52ef63ac151bd09ba21954daecb203ba07e4d
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-07-20 14:36:30 +0530

    footer links

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -10,25 +10,27 @@ import { Link as ScrollLink } from "react-scroll";
 
 const Footer = () => {
   return (
-    <footer className="text-white w-full bg-blue-500 mt-[16rem] shadow-lg shadow-blue-800">
-      <div className="container mx-auto py-16 px-4 md:px-8 font-poppins">
+    <footer className="text-white w-full bg-blue-600 mt-[16rem]">
+      <div className="container mx-auto py-16 px-4 md:px-8">
         <div className="flex flex-col md:flex-row justify-between items-start">
           <div className="w-full sm:w-1/2 lg:w-1/3 mb-8 lg:mb-0 ">
-            <h2 className="text-xl font-bold mb-2 text-white ">
+            <h2 className="text-xl font-bold mb-2 text-blue-100 underline">
               Connect With Us
             </h2>
             <div className="flex flex-col">
               <a
-                href="https://github.com/homiestech"
+                href="https://github.com/homiesTech"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-black p-3 hover:shadow-pink-200 transition duration-300 hover:text-white"
+                className="text-white p-3 hover:shadow-pink-200 transition duration-300"
               >
                 <InstagramIcon /> Instagram
               </a>
               <a
-                href="https://github.com/homiestech"
-                className="text-black p-3 hover:shadow-pink-200 transition duration-300 hover:text-white"
+                href="https://github.com/homiesTech"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white p-3 hover:shadow-pink-200 transition duration-300"
               >
                 <GitHubIcon /> GitHub
               </a>
@@ -36,7 +38,7 @@ const Footer = () => {
                 href="https://www.linkedin.com/in/homiesTech/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-black p-3 hover:shadow-pink-200 transition duration-300 hover:text-white"
+                className="text-white p-3 hover:shadow-pink-200 transition duration-300"
               >
                 <LinkedInIcon /> LinkedIn
               </a>
@@ -44,21 +46,23 @@ const Footer = () => {
                 href="https://twitter.com/homiesTech"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-black p-3 hover:shadow-pink-200 transition duration-300 hover:text-white"
+                className="text-white p-3 hover:shadow-pink-200 transition duration-300"
               >
                 <XIcon /> Twitter
               </a>
             </div>
           </div>
           <div className="w-full sm:w-1/2 lg:w-1/3 mb-8 lg:mb-0">
-            <h2 className="text-xl font-bold mb-2 text-white ">Quick Links</h2>
+            <h2 className="text-xl font-bold mb-2 text-blue-100 underline">
+              Quick Links
+            </h2>
             <ul>
               <li className="mb-2 p-2">
                 <ScrollLink
                   to="home"
                   smooth={true}
                   duration={500}
-                  className="text-black hover:underline hover:text-white"
+                  className="text-white underline"
                   spy={true}
                 >
                   Home
@@ -69,7 +73,7 @@ const Footer = () => {
                   to="about"
                   smooth={true}
                   duration={500}
-                  className="text-black hover:underline hover:text-white"
+                  className="text-white underline"
                   spy={true}
                 >
                   About
@@ -80,7 +84,7 @@ const Footer = () => {
                   to="features"
                   smooth={true}
                   duration={500}
-                  className="text-black hover:underline hover:text-white"
+                  className="text-white underline"
                   spy={true}
                 >
                   Features
@@ -91,7 +95,7 @@ const Footer = () => {
                   to="contact"
                   smooth={true}
                   duration={500}
-                  className="text-black hover:underline hover:text-white"
+                  className="text-white underline"
                   spy={true}
                 >
                   Contact
@@ -100,22 +104,32 @@ const Footer = () => {
             </ul>
           </div>
           <div className="w-full sm:w-1/2 lg:w-1/3">
-            <h2 className="text-xl font-bold mb-2 text-white ">Address</h2>
-            <p className="text-black mb-4 flex items-center">
+            <h2 className="text-xl font-bold mb-2 text-blue-100 underline">
+              Address
+            </h2>
+            <p className="text-white mb-4 flex items-center">
               <HomeIcon className="mr-2 m-2" />
               SCO 106,1st Floor, Hermitage Office, Dhakoli-140603
             </p>
-            <p className="text-black mb-4 flex items-center">
+            <p className="text-white mb-4 flex items-center">
               <EmailIcon className="mr-2 m-2" />
-              Homenetics_Official@homenetics.in
+
+              <a
+                href="mailto:Homenetics_Official@homenetics.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white p-3 hover:shadow-pink-200 transition duration-300"
+              >
+                Homenetics_Official@homenetics.in
+              </a>
             </p>
-            <p className="text-black mb-4 flex items-center">
+            <p className="text-white mb-4 flex items-center">
               <PhoneIcon className="mr-2 m-2" />
               9518499946
             </p>
           </div>
         </div>
-        <div className="text-center text-blue-100 text-sm mt-8">
+        <div className="text-center text-black text-sm mt-8">
           &copy; 2024 Homenetics. All rights reserved.
         </div>
       </div>
